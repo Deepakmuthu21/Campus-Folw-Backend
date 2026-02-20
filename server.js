1c5c5d273856e90da8473435b1686c2db92d8057
Deepakmuthu21 <deepakmuthu21@gmail.com>
2026-02-20 14:30:19 +0530

    update origin

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cors({
   origin: [
     //"http://localhost:3000", // for local testing
-    "https://react-campusflow.netlify.app/" // deployed frontend
+    "https://react-campusflow.netlify.app" // deployed frontend
   ],
   credentials: true
 }
